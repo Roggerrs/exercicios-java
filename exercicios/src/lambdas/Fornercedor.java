f2572146b0f85343aaefdf3b9554e8b4fab3da0f
Roger <Roger@PC_Da_Dignidade>
2024-08-21 01:43:43 -0300

    21 08 2024

---
+++ exercicios/src/lambdas/Fornercedor.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornercedor {
+
+	public static void main(String[] args) {
+		
+		Supplier< List<String>> umLista = 
+				() -> Arrays.asList("Ana", "Bia", "Lia", "Gui");
+				
+				System.out.println(umLista.get());
+	}
+	
+}
