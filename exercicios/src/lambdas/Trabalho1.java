26470ca2db3b47b602b7d0b03b51674292139bb7
Roger <Roger@PC_Da_Dignidade>
2024-09-04 01:40:54 -0300

    t2 04 09 2024

---
+++ exercicios/src/lambdas/Trabalho1.java
@@ -0,0 +1,18 @@
+package lambdas;
+
+
+public class Trabalho1 implements Runnable{
+	
+	public  void run()  {
+		 for (int i = 0; i < 100; i++) {
+			 System.out.println("Tarefa #01");
+			 try {
+				 Thread.sleep(100);
+			 } catch (Exception e) {
+				
+			}
+			 }
+		 }
+	}
+
+
